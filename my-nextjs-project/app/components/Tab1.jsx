5e0683b5511339de181ce06e93e29de4913a3420
mmakboub <makboubmariam@gmail.com>
2024-04-16 20:05:39 +0200

    editing

--- my-nextjs-project/app/components/Tab1.jsx
+++ my-nextjs-project/app/components/Tab1.jsx
@@ -1,14 +1,16 @@
 import { VscArrowLeft } from "react-icons/vsc";
 import { BsTable } from "react-icons/bs";
 import { CiCirclePlus } from "react-icons/ci";
-
+import Image from "next/image";
 function Tab1() {
   return (
     <div className="w-full p-5 h-full flex flex-col">
       <div className="w-full h-[15%] flex justify-between items-center">
         <div className="flex items-center">
-          <VscArrowLeft />
-          <button className="ml-2">Groups</button>
+          <VscArrowLeft style={{ fontSize: "30px", color: "#cbd5e1" }} />
+          <button className="ml-2 text-[#cbd5e1] text-3xl font-semibold">
+            Groups
+          </button>
         </div>
         <div>
           <BsTable />
@@ -16,24 +18,115 @@ function Tab1() {
       </div>
       <div className="w-full h-full pt-10">
         <div className="grid grid-cols-4 gap-4 w-full h-full">
-          <div className="bg-[#0f172a] flex flex-col items-center  rounded-xl gap-3   h-40 w-40 justify-center">
-            <CiCirclePlus className="h-12 w-12" />
+          <div className="bg-[#2f3846] flex flex-col items-center  rounded-xl gap-3   h-40 w-40 justify-center">
+            <div className="flex items-center justify-center w-[40%] h-[40%] rounded-full bg-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="#cbd5e1"
+                className="w-8 h-8"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
+            </div>
+
             <h1 className="text-[#64748b]">Create Group</h1>
           </div>
-          <div className="bg-[#0f172a] flex items-center justify-center rounded-xl  h-40 w-40">
-            01
+          <div className="bg-[#0f172a] flex items-center flex-col justify-center pl-3 rounded-xl  h-40 w-40">
+            <div className="rounded-full h-20 w-20 flex items-center justify-center bg-gray-200 mr-4">
+              <Image
+                src="/tree-736885_640.jpg"
+                alt="Image"
+                width={100}
+                height={100}
+                className="h-full rounded-full"
+              />
+            </div>
+            <div>
+              <p className="text-lg text-white">Cuisine</p>
+              <p className="text-lg text-white">35 member</p>
+            </div>
           </div>
-          <div className="bg-[#0f172a] flex items-center justify-center   rounded-xl h-40 w-40">
-            01
+          <div className="bg-[#0f172a] flex items-center flex-col justify-center pl-3 rounded-xl  h-40 w-40">
+            <div className="rounded-full h-20 w-20 flex items-center justify-center bg-gray-200 mr-4">
+              <Image
+                src="/tree-736885_640.jpg"
+                alt="Image"
+                width={100}
+                height={100}
+                className="h-full rounded-full"
+              />
+            </div>
+            <div>
+              <p className="text-lg text-white">Art</p>
+              <p className="text-lg text-white">9 member</p>
+            </div>
           </div>
-          <div className="bg-[#0f172a] flex items-center justify-center  rounded-xl   h-40 w-40">
-            01
+          <div className="bg-[#0f172a] flex items-center pl-3 justify-center flex-col  rounded-xl h-40 w-40">
+            <div className="rounded-full h-20 w-20 flex items-center justify-center bg-gray-200 mr-4">
+              <Image
+                src="/tree-736885_640.jpg"
+                alt="Image"
+                width={100}
+                height={100}
+                className="h-full rounded-full"
+              />
+            </div>
+            <div>
+              <p className="text-lg text-white">Workout</p>
+              <p className="text-lg text-white">27 member</p>
+            </div>
           </div>
-          <div className="bg-[#0f172a] flex items-center justify-center  rounded-xl  h-40 w-40">
-            01
+          <div className="bg-[#0f172a] flex items-center justify-center pl-3 flex-col rounded-xl   h-40 w-40">
+            <div className="rounded-full h-20 w-20 flex items-center justify-center bg-gray-200 mr-4">
+              <Image
+                src="/tree-736885_640.jpg"
+                alt="Image"
+                width={100}
+                height={100}
+                className="h-full rounded-full"
+              />
+            </div>
+            <div>
+              <p className="text-lg text-white">gaming</p>
+              <p className="text-lg text-white">105 member</p>
+            </div>
           </div>
-          <div className="bg-[#0f172a] flex items-center justify-center  rounded-xl  h-40 w-40">
-            09
+          <div className="bg-[#0f172a] flex items-center justify-center flex-col pl-3 rounded-xl  h-40 w-40">
+            <div className="rounded-full h-20 w-20 flex items-center justify-center bg-gray-200 mr-4">
+              <Image
+                src="/tree-736885_640.jpg"
+                alt="Image"
+                width={100}
+                height={100}
+                className="h-full rounded-full"
+              />
+            </div>
+            <div>
+              <p className="text-lg text-white">Hiking</p>
+              <p className="text-lg text-white">97 member</p>
+            </div>
+          </div>
+          <div className="bg-[#0f172a] flex items-center justify-center flex-col pl-3 rounded-xl  h-40 w-40">
+            <div className="rounded-full h-20 w-20 flex items-center justify-center bg-gray-200 mr-4">
+              <Image
+                src="/tree-736885_640.jpg"
+                alt="Image"
+                width={100}
+                height={100}
+                className="h-full rounded-full"
+              />
+            </div>
+            <div>
+              <p className="text-lg text-white">Yoga</p>
+              <p className="text-lg text-white">65 member</p>
+            </div>
           </div>
         </div>
       </div>
--- my-nextjs-project/app/page.js
+++ my-nextjs-project/app/page.js
@@ -4,7 +4,8 @@ import { IoSettings } from "react-icons/io5";
 
 import Tab1 from "./components/Tab1";
 import Tab2 from "./components/Tab2";
-import React, { useState } from "react"; // Don't forget to import React for using hooks
+import React, { useState } from "react";
+
 export default function Home() {
   const [activeTab, setActiveTab] = useState("tab1");
 
@@ -13,13 +14,13 @@ export default function Home() {
   };
 
   return (
-    <main className="flex justify-center bg-[#0f172a] items-center h-screen">
-      <div className="w-1/2 h-3/4  bg-[#334155] rounded-lg">
-        <div className="w-full h-full flex flex-row gap-5  p-6 rounded-xl">
+    <main className="flex justify-center bg-[#0a111f] items-center h-screen">
+      <div className="w-1/2 h-3/4 bg-[#202c3a] rounded-xl overflow-hidden">
+        <div className="w-full h-full flex flex-row gap-5 p-6 rounded-xl overflow-x-auto overflow-y-auto">
           {/* styling & handling Vertical tabs */}
-          <div className=" bg-[#0f172a] flex justify-center flex-col w-[10%] rounded-xl  ">
+          <div className="bg-[#0f172a] flex justify-center flex-col p-3 w-[10%] rounded-[20px]">
             <button
-              className={`flex h-1/2 w-full items-center  font-vietnam  text-xl pl-7 font-bold text-white  ${
+              className={`flex h-1/2 w-full items-center font-vietnam text-xl  font-bold text-white ${
                 activeTab === "tab1" ? "bg-[#64748b]" : ""
               }`}
               onClick={() => handleTabClick("tab1")}
@@ -27,7 +28,7 @@ export default function Home() {
               <BsFillPeopleFill />
             </button>
             <button
-              className={`flex h-1/2  w-full items-center   font-vietnam  text-xl pl-7 font-bold text-white  ${
+              className={`flex h-1/2 w-full items-center font-vietnam text-xl pl-7 font-bold text-white ${
                 activeTab === "tab2" ? "bg-[#64748b]" : ""
               }`}
               onClick={() => handleTabClick("tab2")}
@@ -36,7 +37,7 @@ export default function Home() {
             </button>
           </div>
           {/* Rendering Content */}
-          <div className=" w-full  h-[full] rounded-xl">
+          <div className="w-full h-full rounded-xl">
             {activeTab === "tab1" && <Tab1 />}
             {activeTab === "tab2" && <Tab2 />}
           </div>
